4cf2ce4247d7325619841aa5ddb27fa5fdc3de5c
varun-s22 <93476421+varun-s22@users.noreply.github.com>
2022-11-13 23:33:54 +0530

    api deploy

--- server/api/index.py
+++ server/api/index.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_cors import CORS
 import subprocess as sp
+import uuid
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -8,11 +10,13 @@ CORS(app)
 @app.route("/code", methods=["POST"])
 def format_code():
     recieved_code=request.get_json()["code"]
-    with open("formatCode.py","w") as external_file:
+    filename = str(uuid.uuid4())
+    with open(f'{filename}.py',"w") as external_file:
         print(recieved_code,file=external_file)
-    formatted_code=sp.getoutput("black formatCode.py 2>> /dev/null;cat formatCode.py")
+    formatted_code=sp.getoutput(f'black {filename}.py 2>> /dev/null;cat {filename}.py')
+    os.remove(f'{filename}.py')
     return {"formattedCode":formatted_code}
 
 
 if (__name__ == "__main__"):
-    app.run()
+    app.run(debug=True)
